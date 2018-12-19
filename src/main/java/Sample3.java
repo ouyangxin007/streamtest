e6b3f9cb4dab03f989c237a20a827253714c2cb0
chixue <334548450@qq.com>
2018-12-19 08:48:07 +0800

    初始版本

--- src/main/java/Sample3.java
+++ src/main/java/Sample3.java
@@ -1,10 +1,7 @@
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -12,7 +9,7 @@ import java.util.stream.Collectors;
 /**
  * Created by qiuzhanghua on 2017/2/7.
  */
-public class Sample1 {
+public class Sample3 {
   public static void main(String[] args) throws IOException {
     final String inputFileName = "sample1.txt";
     final String outputFileName = "sample1.5.txt";
